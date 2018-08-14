c8a0e83fd38dffae68afb282a04c8e9168acfd97
rgaritta <fender8@bellsouth.net>
2018-08-14 17:53:25 -0400

    Not nearly done, servers work, need to flesh out code for logic and question form

---
+++ app/data/bands.js
@@ -0,0 +1,17 @@
+var bands = [
+    {
+      name: "Tame Impala",
+      scores: [
+        "5",
+        "2",
+        "1",
+        "3",
+        "1"
+      ]
+    },
+    
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = bands;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+
+module.exports = function(app) {
+    /*
+    Psuedocode 
+    Write the routes using get and post for the apis. 
+    Include functionality for the logic of the app.
+    Compile the scores and choose a band based on user input.
+    Send the information to be displayed via a modal.
+    */
+}
