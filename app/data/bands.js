31b8fe7530106cd133995175ed40390f650ff9aa
rgaritta <fender8@bellsouth.net>
2018-08-16 17:48:26 -0400

    need to add algorithm and matching

--- app/data/bands.js
+++ app/data/bands.js
@@ -6,11 +6,51 @@ var bands = [
         "2",
         "1",
         "3",
+        "2"
+      ]
+    },
+    {
+      name: "Kanye West",
+      scores: [
+        "2",
+        "5",
+        "3",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: "Oceansize",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
         "1"
       ]
     },
-    
-  
+    {
+      name: "Daedelus",
+      scores: [
+        "2",
+        "4",
+        "5",
+        "2",
+        "1"
+      ]
+    },
+    {
+      name: "Sufjan Stevens" ,
+      scores: [
+        "4",
+        "2",
+        "2",
+        "4",
+        "2"
+      ]
+    },
   ];
+
+  
   
   module.exports = bands;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,7 @@
 var path = require("path");
 
+var bands = require("../data/bands");
+
 
 module.exports = function(app) {
     /*
@@ -9,4 +11,17 @@ module.exports = function(app) {
     Compile the scores and choose a band based on user input.
     Send the information to be displayed via a modal.
     */
+
+   app.get("/api/bands", function(req, res) {
+    res.json(bands);
+  });
+
+  app.post("/api/bands", function(req, res) {
+    //need to add algorithm and matching
+
+
+  });
+
+
+
 }
